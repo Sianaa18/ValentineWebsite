6795a713471eb0065b1ac2a3d9bbc5e2da5dd4a1
Sianaa18 <134985869+Sianaa18@users.noreply.github.com>
2025-02-14 00:08:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const image = document.querySelector(".image");
+const audio = new Audio("blue.mp3");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
     // Hide the No button
     noBtn.style.display = "none";
-    question.innerHTML = "Yay! I love you, Khongor💙.";
-    image.src = "IMG_2550.JPG";
+    yesBtn.style.display = "none";
+    question.innerHTML = "Yay! I knew it😊. Loving you is BLUE💙";
+    image.src = "IMG_2.JPG";
+    audio.play();
 });
 
 // Make the No button move randomly on hover
