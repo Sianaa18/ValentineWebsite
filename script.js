4bd4d531a7105cdbcec81b3a2fa9b363a98053ed
Sianaa18 <134985869+Sianaa18@users.noreply.github.com>
2025-02-13 23:24:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
+const image = document.querySelector(".JPG");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "Being with you is my biggest blessing. I love you.";
-    gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGNhdXh1b252b2F2b2U4cHRlNGkwMDZsajllaGF1cDJyb2p4NXl2YiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/G6N0pDDgDpLjUvNoyQ/giphy.gif";
+    image.src = "IMG_2550.JPG";
 
     // Hide the No button
     noBtn.style.display = "none";
